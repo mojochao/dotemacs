134a634d2007181bf3dc382ea9ad85486a5d312f
Allen Gooch <allen.gooch@gmail.com>
2012-11-15 12:37:03 -0800

    gotta start somewhere

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wombat))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(compilation-info ((t (:inherit success :foreground "xb294bb"))))
+ '(hl-line ((t (:background "dark slate gray"))))
+ '(p4-depot-added-face ((t (:foreground "deep sky blue"))) t)
+ '(rst-level-1-face ((t nil)) t)
+ '(underline ((t (:foreground "xb294bb" :underline t))))
+ '(whitespace-indentation ((t (:foreground "firebrick"))) t)
+ '(whitespace-space ((t (:foreground "SlateGray4"))) t))
---
+++ init.el
@@ -0,0 +1,53 @@
+;; configure customization data to be persisted outside of the .emacs file
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
+;; configure backups to be persisted outside of source directories when editing
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+
+;; configure stfu
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+
+;; configure global ui settings
+
+(setq frame-title-format '("%S" (buffer-file-name "%f" (dired-directory dired-directory "%b")))) ; display full path of active buffer in window title area
+(setq eval-expression-print-length nil) ; prevent emacs truncation of messages in the echo area
+(setq visible-bell t)                   ; flash frame instead of audible bell on error
+(setq column-number-mode t)             ; show column numbers in modeline
+(setq line-number-mode t)               ; show line numbers in modeline
+(delete-selection-mode 1)               ; delete marked region and replace with new content
+(scroll-bar-mode -1)                    ; hide scroll bars
+(menu-bar-mode -1)                      ; hide menu bar
+(tool-bar-mode -1)                      ; hide tool bar
+(tooltip-mode -1)                       ; hide tool tips
+(fset 'yes-or-no-p 'y-or-n-p)           ; use shortcuts for all yes/no prompts
+
+(require 'dired-x)
+(global-set-key (kbd "C-x C-n") 'find-name-dired)
+
+(require 'ido)
+(setq ido-enable-flex-matching t)
+(ido-mode t)
+(ido-everywhere t)
+
+(require 'recentf)
+(recentf-mode t)
+(setq recentf-max-saved-items 50)
+(defun ido-recentf-open ()
+  "Use `ido-completing-read' to \\[find-file] a recent file"
+  (interactive)
+  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+      (message "Opening file...")
+    (message "Aborting")))
+
+(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
+
+
+;; (defconst ace-jump-mode-package-path (concat elisp-packages-path "/ace-jump-mode"))
+;; (add-to-list 'load-path ace-jump-mode-package-path)
+;; (require 'ace-jump-mode)
+;; (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
