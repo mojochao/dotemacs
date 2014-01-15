cfa9e3cd0fdb69ccf30178d320c4ccd78fc1b149
Allen Gooch <allen.gooch@gmail.com>
2014-01-15 17:14:21 -0600

    PlantUML support

--- init.el
+++ init.el
@@ -43,6 +43,7 @@
 
 (defvar vendor-packages '(js2-mode
 			  magit
+			  plantuml-mode
 			  web-mode
 			  yasnippet
 			  zenburn-theme)
@@ -174,16 +175,51 @@ Then move to that line and indent accordning to mode"
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-
-
 (add-hook 'js2-mode-hook (lambda () 
 			   (setq indent-tabs-mode nil)
 			   (setq js2-basic-offset 2)  
 			   (setq js2-bounce-indent-p t)))
-
 (add-hook 'js2-mode-hook 'my-run-pmh-if-not-ran)
+
 (defun my-run-pmh-if-not-ran ()
   (unless (bound-and-true-p my-pmh-ran)
     (run-hooks 'prog-mode-hook)))
 
+;;------------------------------------------------------------------------------
+;;
+;; PlantUML IDE
+;;
+;;------------------------------------------------------------------------------
 
+(require 'plantuml-mode)
+(add-to-list 'auto-mode-alist '("\\.pu\\'" . plantuml-mode))
+(require 'iimage)
+(autoload 'iimage-mode "iimage" "Support Inline image minor mode." t)
+(autoload 'turn-on-iimage-mode "iimage" "Turn on Inline image minor mode." t)
+(add-to-list 'iimage-mode-image-regex-alist '("@startuml\s+\\(.+\\)" . 1))
+ 
+;; Rendering plantuml
+(defun plantuml-render-buffer ()
+  (interactive)
+  (message "PLANTUML Start rendering")
+  (shell-command (concat "java -jar ~/plantuml.jar " 
+                         buffer-file-name))
+  (message (concat "PLANTUML Rendered:  " (buffer-name))))
+ 
+;; Image reloading
+(defun reload-image-at-point ()
+  (interactive)
+  (message "reloading image at point in the current buffer...")
+  (image-refresh (get-text-property (point) 'display)))
+ 
+;; Image resizing and reloading
+(defun resize-image-at-point ()
+  (interactive)
+  (message "resizing image at point in the current buffer123...")
+  (let* ((image-spec (get-text-property (point) 'display))
+         (file (cadr (member :file image-spec))))
+    (message (concat "resizing image..." file))
+    (shell-command (format "convert -resize %d %s %s " 
+                           (* (window-width (selected-window)) (frame-char-width))
+                           file file))
+    (reload-image-at-point)))
