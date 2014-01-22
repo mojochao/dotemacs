a639aa12d5d9b082fe1b6397b26c6da7ab4423c8
Allen Gooch <allen.gooch@gmail.com>
2014-01-22 17:34:21 -0600

    made auto-reverting of images actually work

--- init.el
+++ init.el
@@ -193,33 +193,7 @@ Then move to that line and indent accordning to mode"
 
 (require 'plantuml-mode)
 (add-to-list 'auto-mode-alist '("\\.pu\\'" . plantuml-mode))
-(require 'iimage)
-(autoload 'iimage-mode "iimage" "Support Inline image minor mode." t)
-(autoload 'turn-on-iimage-mode "iimage" "Turn on Inline image minor mode." t)
-(add-to-list 'iimage-mode-image-regex-alist '("@startuml\s+\\(.+\\)" . 1))
- 
-;; Rendering plantuml
-(defun plantuml-render-buffer ()
-  (interactive)
-  (message "PLANTUML Start rendering")
-  (shell-command (concat "java -jar ~/plantuml.jar " 
-                         buffer-file-name))
-  (message (concat "PLANTUML Rendered:  " (buffer-name))))
- 
-;; Image reloading
-(defun reload-image-at-point ()
-  (interactive)
-  (message "reloading image at point in the current buffer...")
-  (image-refresh (get-text-property (point) 'display)))
- 
-;; Image resizing and reloading
-(defun resize-image-at-point ()
-  (interactive)
-  (message "resizing image at point in the current buffer123...")
-  (let* ((image-spec (get-text-property (point) 'display))
-         (file (cadr (member :file image-spec))))
-    (message (concat "resizing image..." file))
-    (shell-command (format "convert -resize %d %s %s " 
-                           (* (window-width (selected-window)) (frame-char-width))
-                           file file))
-    (reload-image-at-point)))
+
+(add-hook 'image-mode-hook (lambda ()
+			     (auto-revert-mode 1)
+			     (auto-image-file-mode +1)))
